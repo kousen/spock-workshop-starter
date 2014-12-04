f7a0c6198957ddc35b65440f7c45b752ce432ca5
Kenneth Kousen <ken.kousen@kousenit.com>
2014-12-04 12:34:49 -0500

    Clean up tests

--- src/test/groovy/mjg/MyFirstSpec.groovy
+++ src/test/groovy/mjg/MyFirstSpec.groovy
@@ -3,12 +3,18 @@ package mjg
 import spock.lang.Specification
 
 class MyFirstSpec extends Specification {
-    def "max of two numbers"() {
+    def "Math.max of 1 and 2 is 2"() {
         expect:
         Math.max(1, 2) == 2
     }
 
     // Add a similar test for Math.min
+    void 'The minimum of -2 and -6 is -6'() {
+        expect:
+        Math.min(-2, -6) == -6
+        Math.min(0, 0) == 0
+        Math.min(Integer.MIN_VALUE, 0) == Integer.MIN_VALUE
+    }
 
     // Add a test for Math.abs
 }
